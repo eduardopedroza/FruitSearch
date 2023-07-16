fd8ded469ced3f3bc7c26d33b2ce40cb4895dde6
eduardopedroza <131719448+eduardopedroza@users.noreply.github.com>
2023-07-16 10:47:22 -0600

    Updated script.js
    Cleaned up code and fixed the bold function

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
 	//takes the fruit array and filters out each item that contains the users input
-	results = fruit.filter(items => {
+	return results = fruit.filter(items => {
 		return items.toLowerCase().includes(str.toLowerCase());
 	})
-	return results;
 }
 
 function searchHandler(e) {
@@ -36,22 +33,32 @@ function searchHandler(e) {
 function showSuggestions(results, inputVal) {
 	suggestions.innerHTML = '';
 	results.forEach(value => {
-		let li = document.createElement("li");
-		li.innerText = value;
-
-		//make inputVal bold
-
-		// let index = value.indexOf(inputVal.toLowerCase());
-		// let firstPart = value.substring(0, index);
-		// let boldedText = value.toLowerCase().substring(index, index + inputVal.length);
-		// let remainingText = value.substring(index + inputVal.length);
-
-		// li.innerHTML = `${firstPart}<b>${boldedText}</b>${remainingText}`;
-
+		let li = document.createElement("li");		
+		li.innerHTML = bold(value, inputVal);
 		suggestions.append(li);
 	})
 }
 
+function bold(value, inputVal){
+	let firstPart = '';
+	let boldedText = '';
+	let remainingText = '';
+
+	let index = value.toLowerCase().indexOf(inputVal.toLowerCase());
+
+	if(index === 0){
+		boldedText = value.substring(0, inputVal.length);
+		remainingText = value.substring(inputVal.length, value.length);
+		return `<b>${boldedText}</b>${remainingText}`
+	}
+	else {
+		firstPart = value.substring(0, index);
+		boldedText = value.toLowerCase().substring(index, index + inputVal.length);
+		remainingText = value.substring(index + inputVal.length);
+
+		return `${firstPart}<b>${boldedText}</b>${remainingText}`;
+	}
+}	
 
 function useSuggestion(e) {
 	let str = e.target.innerHTML;
